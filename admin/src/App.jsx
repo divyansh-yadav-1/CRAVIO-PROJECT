407a16a690a034770ca48b50c25524ceb7bd7705
Divyansh Yadav <158609746+divyansh-yadav-1@users.noreply.github.com>
2025-07-09 09:58:55 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url="http://localhost:4000"; // Change this to your backend URL if needed
+  const url="https://cravio-backend.onrender.com"; // Change this to your backend URL if needed
   return (
     <div>
       <ToastContainer/>
